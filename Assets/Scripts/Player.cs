8da2e9a81aaa15a8383f3fb2ee4c5fc6896f5055
Burikkkk <sandra.k.buraya@gmail.com>
2025-02-27 18:30:29 +0300

    animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
 
 public class Player : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class Player : MonoBehaviour
     [SerializeField] private float speed = 3f;
     [SerializeField] private int hp = 30;
     [SerializeField] private float jumpForce = 15f;
+    [SerializeField] private float speedBoost = 2f;
     private float rayLength = 1.0f;
     public LayerMask groundLayer;
 
@@ -26,6 +28,10 @@ public class Player : MonoBehaviour
             Run();
         if (CheckGrounded() && Input.GetKeyDown(KeyCode.Space))
             Jump();
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+            speed *= speedBoost;
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+            speed /= speedBoost;
     }
 
     private void FixedUpdate()
