1a8fbd7d6a2cc33590a2aabb76654c7478625303
Burikkkk <sandra.k.buraya@gmail.com>
2025-02-27 22:12:59 +0300

    animator

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,37 +15,47 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
+    private Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponentInChildren<SpriteRenderer>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
     {
+        if (CheckGrounded())
+            State = States.Idle;
         if (Input.GetButton("Horizontal"))
             Run();
         if (CheckGrounded() && Input.GetKeyDown(KeyCode.Space))
             Jump();
-        if (Input.GetKeyDown(KeyCode.Mouse1))
+        if (Input.GetKeyDown(KeyCode.Mouse1) || Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (CheckGrounded()) State = States.Slide;
             speed *= speedBoost;
-        if (Input.GetKeyUp(KeyCode.Mouse1))
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse1) || Input.GetKeyUp(KeyCode.LeftShift))
             speed /= speedBoost;
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            Attack();
     }
 
-    private void FixedUpdate()
-    {
-        //CheckGrounded();
-    }
+
 
 
     private void Run()
     {
+        if (CheckGrounded()) State = States.Run;
+
         Vector3 dir = transform.right * Input.GetAxis("Horizontal");
         transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
-        sprite.flipX = dir.x < 0.0f;    
+        sprite.flipX = dir.x < 0.0f;
     }
+
+
 
     private bool CheckGrounded()
     {
@@ -55,6 +65,31 @@ public class Player : MonoBehaviour
     private void Jump()
     {
         rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        if (!CheckGrounded()) State = States.Jump;
 
+    }
+
+    private void Attack()
+    {
+        State = States.Attack;
+    }
+
+    public enum States
+    {
+
+        Idle, 
+        Run,
+        Slide,
+        Attack,
+        HurtNoEffect,
+        DeathNoEffect,
+        Jump
+    }
+
+    private States State
+    {
+        get { return (States)animator.GetInteger("State"); }
+        set { animator.SetInteger("State", (int)value); }
+  
     }
 }
